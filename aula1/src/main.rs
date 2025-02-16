054634662e3da894e01594b01d8ec3135ec098f5
ViniGRocha <106942837+ViniGRocha@users.noreply.github.com>
2025-02-16 14:59:41 +0000

    Criação de função para inverter palavras

--- aula1/src/main.rs
+++ aula1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// Função para inverter uma String
+
+fn reverse(palavra: &str) -> String { // Essa linha está declarando que o tipo da função vai receber o dado do tipo String
+    palavra.chars().rev().collect() // Essa linha está invertendo o dado recebido pela função "palavra"
+}
+
+
+fn main(){
+    let palavra = "Giovanna Andrade"; // Essa linha define a palavra que será invertida (no caso a frase) 
+    let palavra_invertida = reverse(palavra); // Aqui chamamos a função "palavra" e armazenamos na variavel "palavra_invertida"
+
+    println!("Palavra(as) Invertida(as): {}", palavra_invertida);
 }
